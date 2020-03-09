7c4577ef64a3ea42daacf0bf9e0dcf250a475ce6
Kazuki Yoshida <kazukiyoshida@mail.harvard.edu>
2020-03-09 13:41:31 -0400

    Add and document model fitter functions

--- R/internal_functions.R
+++ R/internal_functions.R
@@ -33,7 +33,7 @@ new_regmedint <- function(data,
     mreg_fit <- fit_mreg(mreg, data, avar, mvar, cvar)
 
     ## Perform yreg
-    yreg_fit <- fit_yreg(yreg, data, yvar, avar, mvar, cvar, eventvar)
+    yreg_fit <- fit_yreg(yreg, data, yvar, avar, mvar, cvar, interaction, eventvar)
 
     ## Construct the result object
     res <- list(mreg = mreg_fit,
@@ -46,4 +46,54 @@ new_regmedint <- function(data,
 
     ##
     res
+}
+
+
+##' Fit a model for the mediator given the treatment and covariates.
+##'
+##' \code{\link{lm}} is called if \code{mreg = "linear"}. \code{\link{glm}} is called with \code{family = binomial()} if \code{mreg = "logistic"}.
+##'
+##' @inheritParams regmedint
+##'
+##' @return A regression object of class lm (linear) or glm (logistic)
+fit_mreg <- function(mreg,
+                     data,
+                     avar,
+                     mvar,
+                     cvar) {
+
+}
+
+
+## The third and subsequent paragraphs go into details.
+## http://r-pkgs.had.co.nz/man.html#roxygen-comments
+
+##' Fit a model for the outcome given the treatment, mediator, and covariates.
+##'
+##' The outcome model type \code{yreg} can be one of the following \code{"linear"}, \code{"logistic"}, \code{"loglinear"}, \code{"poisson"}, \code{"negbin"}, \code{"survCox"}, \code{"survAFT_exp"}, or \code{"survAFT_weibull"}.
+##'
+##' The outcome regression functions to be called are the following:
+##' \itemize{
+##'   \item \code{"linear"} \code{\link{lm}}
+##'   \item \code{"logistic"} \code{\link{glm}}
+##'   \item \code{"loglinear"} \code{\link{glm}}
+##'   \item \code{"poisson"} \code{\link{glm}}
+##'   \item \code{"negbin"} \code{\link[MASS]{glm.nb}}
+##'   \item \code{"survCox"} \code{\link[survival]{coxph}}
+##'   \item \code{"survAFT_exp"} \code{\link[survival]{survreg}}
+##'   \item \code{"survAFT_weibull"} \code{\link[survival]{survreg}}
+##' }
+##'
+##' @inheritParams regmedint
+##'
+##' @return
+fit_yreg <- function(yreg,
+                     data,
+                     yvar,
+                     avar,
+                     mvar,
+                     cvar,
+                     interaction,
+                     eventvar) {
+
 }
