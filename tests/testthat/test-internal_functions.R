6ae3528867c0c26f6170143f1551d3102bb58c06
Kazuki Yoshida <kazukiyoshida@mail.harvard.edu>
2020-03-09 14:55:16 -0400

    Fix test for formula creators to avoid reliance on default
    - Avoid default arguments in internal functions.

--- tests/testthat/test-internal_functions.R
+++ tests/testthat/test-internal_functions.R
@@ -30,45 +30,54 @@ test_that("string_mreg_formula create sound formula strings", {
 test_that("string_yreg_formula create sound formula strings for non-survival outcomes", {
 
     ## Zero covariates
-    expect_equal(string_yreg_formula("Y","A","M",NULL, interaction = FALSE),
+    expect_equal(string_yreg_formula("Y","A","M",NULL, interaction = FALSE,
+                                     eventvar = NULL),
                  "Y ~ A + M")
-    expect_equal(string_yreg_formula("Y","A","M",NULL, interaction = TRUE),
+    expect_equal(string_yreg_formula("Y","A","M",NULL, interaction = TRUE,
+                                     eventvar = NULL),
                  "Y ~ A*M")
 
     ## One covariate
-    expect_equal(string_yreg_formula("Y","A","M",c("C"), interaction = FALSE),
+    expect_equal(string_yreg_formula("Y","A","M",c("C"), interaction = FALSE,
+                                     eventvar = NULL),
                  "Y ~ A + M + C")
-    expect_equal(string_yreg_formula("Y","A","M",c("C"), interaction = TRUE),
+    expect_equal(string_yreg_formula("Y","A","M",c("C"), interaction = TRUE,
+                                     eventvar = NULL),
                  "Y ~ A*M + C")
 
     ## Three covariates
-    expect_equal(string_yreg_formula("Y","A","M",c("C1","C2","C3"),
-                                     interaction = FALSE),
+    expect_equal(string_yreg_formula("Y","A","M",c("C1","C2","C3"), interaction = FALSE,
+                                     eventvar = NULL),
                  "Y ~ A + M + C1 + C2 + C3")
-    expect_equal(string_yreg_formula("Y","A","M",c("C1","C2","C3"),
-                                     interaction = TRUE),
+    expect_equal(string_yreg_formula("Y","A","M",c("C1","C2","C3"), interaction = TRUE,
+                                     eventvar = NULL),
                  "Y ~ A*M + C1 + C2 + C3")
 })
+
 
 test_that("string_yreg_formula create sound formula strings for survival outcomes", {
 
     ## Zero covariates
-    expect_equal(string_yreg_formula("time","A","M",NULL,"event", interaction = FALSE),
+    expect_equal(string_yreg_formula("time","A","M",NULL,"event", interaction = FALSE,
+                                     eventvar = NULL),
                  "Surv(time, event) ~ A + M")
-    expect_equal(string_yreg_formula("time","A","M",NULL,"event", interaction = TRUE),
+    expect_equal(string_yreg_formula("time","A","M",NULL,"event", interaction = TRUE,
+                                     eventvar = NULL),
                  "Surv(time, event) ~ A*M")
 
     ## One covariate
-    expect_equal(string_yreg_formula("time","A","M",c("C"),"event", interaction = FALSE),
+    expect_equal(string_yreg_formula("time","A","M",c("C"),"event", interaction = FALSE,
+                                     eventvar = NULL),
                  "Surv(time, event) ~ A + M + C")
-    expect_equal(string_yreg_formula("time","A","M",c("C"),"event", interaction = TRUE),
+    expect_equal(string_yreg_formula("time","A","M",c("C"),"event", interaction = TRUE,
+                                     eventvar = NULL),
                  "Surv(time, event) ~ A*M + C")
 
     ## Three covariates
-    expect_equal(string_yreg_formula("time","A","M",c("C1","C2","C3"),"event",
-                                     interaction = FALSE),
+    expect_equal(string_yreg_formula("time","A","M",c("C1","C2","C3"), interaction = FALSE,
+                                     eventvar = "event"),
                  "Surv(time, event) ~ A + M + C1 + C2 + C3")
-    expect_equal(string_yreg_formula("time","A","M",c("C1","C2","C3"),"event",
-                                     interaction = TRUE),
+    expect_equal(string_yreg_formula("time","A","M",c("C1","C2","C3"), interaction = TRUE,
+                                     eventvar = "event"),
                  "Surv(time, event) ~ A*M + C1 + C2 + C3")
 })
