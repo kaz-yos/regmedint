79cb0c4bfab317c2e0eccdc7b2955284fa620945
Kazuki Yoshida <kazukiyoshida@mail.harvard.edu>
2020-03-09 14:49:27 -0400

    Reformat package documentation

--- R/package.R
+++ R/package.R
@@ -8,6 +8,7 @@
 ##' regmedint: A package for regression-based causal mediation analysis
 ##'
 ##' The package is a simple R implementation of the SAS macro as described in Valeri & VanderWeele 2013 and Valeri & VanderWeele 2015 \url{https://www.hsph.harvard.edu/tyler-vanderweele/tools-and-tutorials/}.
+##'
 ##' @section Fitting models
 ##' Use the regmedint function to fit models and conduct regression-based causal mediation analysis.
 ##'
