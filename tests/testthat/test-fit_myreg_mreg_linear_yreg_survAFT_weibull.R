b0d25f83cfdcdeeeed8bf9f96f128b82c1972b4c
Kazuki Yoshida <kazukiyoshida@mail.harvard.edu>
2020-03-10 16:06:40 -0400

    Add template tests for fit_myreg

--- tests/testthat/test-fit_myreg_mreg_linear_yreg_survAFT_weibull.R
+++ tests/testthat/test-fit_myreg_mreg_linear_yreg_survAFT_weibull.R
@@ -12,7 +12,7 @@ library(tidyverse)
 
 
 ###
-### Internal function for yreg model fitting (logistic)
+### Internal function for myreg model fitting
 ################################################################################
 
 test_that("fit_myreg fit linear / Weibull AFT models correctly", {
@@ -39,8 +39,40 @@ test_that("fit_myreg fit linear / Weibull AFT models correctly", {
                           cvar = NULL,
                           interaction = FALSE,
                           eventvar = "status")
-    myreg_fit0 <- fit_myreg()
-
+    myreg_fit0 <- fit_myreg(mreg_fit0, yreg_fit0)
+    ref_fit0 <- tibble(beta_0 = coef(mreg_fit0)["(Intercept)"],
+                       beta_A = coef(mreg_fit0)["trt"],
+                       beta_C = list(0),
+                       sigma = sigma(mreg_fit0),
+                       theta_A = coef(yreg_fit0)["trt"],
+                       theta_M = coef(yreg_fit0)["bili"],
+                       theta_AM = 0,
+                       a1 = 1,
+                       a0 = 0,
+                       m_cde = 0.6,
+                       c_cond = list(1.1),
+                       ## c part of linear predictor by inner product of beta_C and c_cond
+                       clp = map2_dbl(beta_C, c_cond, function(a,b) {sum(a*b)})) %>%
+        ## VanderWeele 2015 p494
+        ## natural effects on log scale
+        mutate(pnde = (theta_A +
+                       theta_AM * ((beta_0 + beta_A * a0 + clp) +
+                                   sigma^2 * (theta_M + 1/2 * theta_AM * (a1 + a0)))) * (a1 - a0),
+               tnie = beta_A * (theta_M + theta_AM * a1) * (a1 - a0),
+               cde_m = (theta_A + theta_AM * m_cde) * (a1 - a0),
+               ## FIXME: Made upg
+               tnde = 999,
+               pnie = 888)
+    expect_equal(myreg_fit0$myreg_fit["pnde"],
+                 ref_fit0$pnde)
+    expect_equal(myreg_fit0$myreg_fit["tnie"],
+                 ref_fit0$tnie)
+    expect_equal(myreg_fit0$myreg_fit["cde_m"],
+                 ref_fit0$cde_m)
+    expect_equal(myreg_fit0$myreg_fit["tnde"],
+                 ref_fit0$tnde)
+    expect_equal(myreg_fit0$myreg_fit["pnie"],
+                 ref_fit0$pnie)
 
     ## One covariates
     yreg_fit1 <- fit_yreg(yreg = "survAFT_weibull",
@@ -51,21 +83,13 @@ test_that("fit_myreg fit linear / Weibull AFT models correctly", {
                           cvar = c("age"),
                           interaction = FALSE,
                           eventvar = "status")
-    ref_fit1 <- survreg(formula = Surv(time,status) ~ trt + bili + age,
-                        dist = "weibull",
-                        data = pbc_cc)
-    ## Same classes
-    expect_equal(class(yreg_fit1),
-                 class(ref_fit1))
-    ## Same formula
-    expect_equal(as.character(yreg_fit1$call$formula),
-                 as.character(ref_fit1$call$formula))
-    ## Same coef
-    expect_equal(coef(yreg_fit1),
-                 coef(ref_fit1))
-    ## Same vcov
-    expect_equal(vcov(yreg_fit1),
-                 vcov(ref_fit1))
+    mreg_fit1 <- fit_mreg(mreg = "linear",
+                          data = pbc_cc,
+                          avar = "trt",
+                          mvar = "bili",
+                          cvar = c("age"))
+    expect_equal(TRUE,
+                 FALSE)
 
     ## Three covariates
     yreg_fit3 <- fit_yreg(yreg = "survAFT_weibull",
@@ -76,21 +100,13 @@ test_that("fit_myreg fit linear / Weibull AFT models correctly", {
                           cvar = c("age","male","stage"),
                           interaction = FALSE,
                           eventvar = "status")
-    ref_fit3 <- survreg(formula = Surv(time,status) ~ trt + bili + age + male + stage,
-                        dist = "weibull",
-                        data = pbc_cc)
-    ## Same classes
-    expect_equal(class(yreg_fit3),
-                 class(ref_fit3))
-    ## Same formula
-    expect_equal(as.character(yreg_fit3$call$formula),
-                 as.character(ref_fit3$call$formula))
-    ## Same coef
-    expect_equal(coef(yreg_fit3),
-                 coef(ref_fit3))
-    ## Same vcov
-    expect_equal(vcov(yreg_fit3),
-                 vcov(ref_fit3))
+    mreg_fit3 <- fit_mreg(mreg = "linear",
+                          data = pbc_cc,
+                          avar = "trt",
+                          mvar = "bili",
+                          cvar = c("age","male","stage"))
+    expect_equal(TRUE,
+                 FALSE)
 
 })
 
@@ -114,21 +130,8 @@ test_that("fit_myreg fit linear / Weibull AFT models correctly with interaction"
                           cvar = NULL,
                           interaction = TRUE,
                           eventvar = "status")
-    ref_fit0 <- survreg(formula = Surv(time,status) ~ trt*bili,
-                        dist = "weibull",
-                        data = pbc_cc)
-    ## Same classes
-    expect_equal(class(yreg_fit0),
-                 class(ref_fit0))
-    ## Same formula
-    expect_equal(as.character(yreg_fit0$call$formula),
-                 as.character(ref_fit0$call$formula))
-    ## Same coef
-    expect_equal(coef(yreg_fit0),
-                 coef(ref_fit0))
-    ## Same vcov
-    expect_equal(vcov(yreg_fit0),
-                 vcov(ref_fit0))
+    expect_equal(TRUE,
+                 FALSE)
 
     ## One covariates
     yreg_fit1 <- fit_yreg(yreg = "survAFT_weibull",
@@ -139,21 +142,8 @@ test_that("fit_myreg fit linear / Weibull AFT models correctly with interaction"
                           cvar = c("age"),
                           interaction = TRUE,
                           eventvar = "status")
-    ref_fit1 <- survreg(formula = Surv(time,status) ~ trt*bili + age,
-                        dist = "weibull",
-                        data = pbc_cc)
-    ## Same classes
-    expect_equal(class(yreg_fit1),
-                 class(ref_fit1))
-    ## Same formula
-    expect_equal(as.character(yreg_fit1$call$formula),
-                 as.character(ref_fit1$call$formula))
-    ## Same coef
-    expect_equal(coef(yreg_fit1),
-                 coef(ref_fit1))
-    ## Same vcov
-    expect_equal(vcov(yreg_fit1),
-                 vcov(ref_fit1))
+    expect_equal(TRUE,
+                 FALSE)
 
     ## Three covariates
     yreg_fit3 <- fit_yreg(yreg = "survAFT_weibull",
@@ -164,20 +154,8 @@ test_that("fit_myreg fit linear / Weibull AFT models correctly with interaction"
                           cvar = c("age","male","stage"),
                           interaction = TRUE,
                           eventvar = "status")
-    ref_fit3 <- survreg(formula = Surv(time,status) ~ trt*bili + age + male + stage,
-                        dist = "weibull",
-                        data = pbc_cc)
-    ## Same classes
-    expect_equal(class(yreg_fit3),
-                 class(ref_fit3))
-    ## Same formula
-    expect_equal(as.character(yreg_fit3$call$formula),
-                 as.character(ref_fit3$call$formula))
-    ## Same coef
-    expect_equal(coef(yreg_fit3),
-                 coef(ref_fit3))
-    ## Same vcov
-    expect_equal(vcov(yreg_fit3),
-                 vcov(ref_fit3))
+    expect_equal(TRUE,
+                 FALSE)
+
 
 })
