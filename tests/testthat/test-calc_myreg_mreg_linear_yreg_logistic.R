010caecdc1ad3c948d6be6703d06bb5596c05fcd
Kazuki Yoshida <kazukiyoshida@mail.harvard.edu>
2020-03-11 10:44:09 -0400

    Add tentative Sigma_sigma_hat_sq

--- tests/testthat/test-calc_myreg_mreg_linear_yreg_logistic.R
+++ tests/testthat/test-calc_myreg_mreg_linear_yreg_logistic.R
@@ -40,22 +40,28 @@ test_that("variance estimates for sigma^2 is extracted", {
     pbc_cc <- pbc[complete.cases(pbc),] %>%
         mutate(male = if_else(sex == "m", 1L, 0L))
 
-
-    ## glm
-    glm_fit <- glm(formula = alk.phos ~ trt + bili,
-                   family  = gaussian(link = "identity"),
-                   data    = pbc_cc)
-    ## summary(glm_fit)$dispersion has no se.
-    expect_equal(dim(vcov(glm_fit)),
-                 ## 4x4 including the dispersion parameter.
-                 c(4,4))
+    ## FIXME: This is tentative and suspicious.
+    ## Chapter 2
+    ## Quadratic Forms of Random Variables
+    ## http://pages.stat.wisc.edu/~st849-1/lectures/Ch02.pdf
+    ## Corollary 6. In a full-rank Gaussian model for M with p covariates X.
+    ## ||M - X beta_hat||^2 / sigma^2 ~ (central chi-squared DF = (n - p))
+    ## Var(central chi-squared DF = (n - p)) = (n - p)
+    ## Var(sigma_hat^2) = Var(1/(n-p) * ||M - X beta_hat||^2)
+    ##                  = 1/(n-p)^2 * Var(||M - X beta_hat||^2 * sigma^2/sigma^2)
+    ##                  = 1/(n-p)^2 * (sigma^2)^2 * Var(||M - X beta_hat||^2 /sigma^2)
+    ##                  = 1/(n-p)^2 * (sigma^2)^2 * (n-p)
+    ##                  = (sigma^2)^2 / (n-p)
 
     ## lm
     lm_fit <- lm(formula = alk.phos ~ trt + bili,
                  data    = pbc_cc)
-    expect_equal(dim(vcov(lm_fit)),
-                 ## 4x4 including the dispersion parameter.
-                 c(4,4))
-
+    ## Just vcov() for lm
+    expect_equal(Sigma_beta_hat(lm_fit),
+                 vcov(lm_fit))
+    ## Derivation above.
+    expect_equal(Sigma_sigma_hat_sq(lm_fit),
+                 ## (sigma_hat^2)^2 / (n-p)
+                 ((sigma(lm_fit))^2)^2 / lm_fit$df.residual)
 
 })
