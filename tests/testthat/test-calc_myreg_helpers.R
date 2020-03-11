b4a0864d3fa0fd3c3cbf36bfff6107e1c31450ab
Kazuki Yoshida <kazukiyoshida@mail.harvard.edu>
2020-03-11 15:33:53 -0400

    Describe Sigma_sigma_hat_sq as giving a 1x1 matrix

--- tests/testthat/test-calc_myreg_helpers.R
+++ tests/testthat/test-calc_myreg_helpers.R
@@ -61,32 +61,36 @@ describe("Sigma_beta_hat", {
     })
 })
 
-test_that("Sigma_sigma_hat_sq extracts the variance estimate for sigma^2", {
+
+describe("Sigma_sigma_hat_sq", {
 
     data(pbc)
     ## Missing data should be warned in validate_args()
     pbc_cc <- pbc[complete.cases(pbc),] %>%
         mutate(male = if_else(sex == "m", 1L, 0L))
 
-    ## FIXME: This is tentative and suspicious.
-    ## Chapter 2
-    ## Quadratic Forms of Random Variables
-    ## http://pages.stat.wisc.edu/~st849-1/lectures/Ch02.pdf
-    ## Corollary 6. In a full-rank Gaussian model for M with p covariates X.
-    ## ||M - X beta_hat||^2 / sigma^2 ~ (central chi-squared DF = (n - p))
-    ## Var(central chi-squared DF = (n - p)) = (n - p)
-    ## Var(sigma_hat^2) = Var(1/(n-p) * ||M - X beta_hat||^2)
-    ##                  = 1/(n-p)^2 * Var(||M - X beta_hat||^2 * sigma^2/sigma^2)
-    ##                  = 1/(n-p)^2 * (sigma^2)^2 * Var(||M - X beta_hat||^2 /sigma^2)
-    ##                  = 1/(n-p)^2 * (sigma^2)^2 * (n-p)
-    ##                  = (sigma^2)^2 / (n-p)
+    it("extracts the variance estimate for sigma^2", {
 
-    ## lm
-    lm_fit <- lm(formula = alk.phos ~ trt + bili,
-                 data    = pbc_cc)
-    ## Derivation above.
-    expect_equal(Sigma_sigma_hat_sq(lm_fit),
-                 ## (sigma_hat^2)^2 / (n-p)
-                 ((sigma(lm_fit))^2)^2 / lm_fit$df.residual)
+        ## FIXME: This is tentative and suspicious.
+        ## Chapter 2
+        ## Quadratic Forms of Random Variables
+        ## http://pages.stat.wisc.edu/~st849-1/lectures/Ch02.pdf
+        ## Corollary 6. In a full-rank Gaussian model for M with p covariates X.
+        ## ||M - X beta_hat||^2 / sigma^2 ~ (central chi-squared DF = (n - p))
+        ## Var(central chi-squared DF = (n - p)) = (n - p)
+        ## Var(sigma_hat^2) = Var(1/(n-p) * ||M - X beta_hat||^2)
+        ##                  = 1/(n-p)^2 * Var(||M - X beta_hat||^2 * sigma^2/sigma^2)
+        ##                  = 1/(n-p)^2 * (sigma^2)^2 * Var(||M - X beta_hat||^2 /sigma^2)
+        ##                  = 1/(n-p)^2 * (sigma^2)^2 * (n-p)
+        ##                  = (sigma^2)^2 / (n-p)
 
+        ## lm
+        lm_fit <- lm(formula = alk.phos ~ trt + bili,
+                     data    = pbc_cc)
+        ## Derivation above.
+        expect_equal(Sigma_sigma_hat_sq(lm_fit),
+                     ## (sigma_hat^2)^2 / (n-p)
+                     matrix(((sigma(lm_fit))^2)^2 / lm_fit$df.residual))
+    })
 })
+
