6f29ae4f0901d93a0f5e623cbaedb6c606e9c75b
Kazuki Yoshida <kazukiyoshida@mail.harvard.edu>
2020-03-12 12:53:55 -0400

    Add coef extractors as copies of vcov extractors

--- R/06_calc_myreg_helpers.R
+++ R/06_calc_myreg_helpers.R
@@ -7,6 +7,76 @@
 
 
 ###
+### coef extractors
+################################################################################
+
+beta_hat <- function(mreg, mreg_fit, avar, cvar) {
+    vars <- c("(Intercept)", avar, cvar)
+    vcov(mreg_fit)[vars,vars, drop = FALSE]
+}
+
+theta_hat <- function(yreg, yreg_fit, avar, mvar, cvar, interaction) {
+
+    vcov_raw <- vcov(yreg_fit)
+
+    if (yreg == "survAFT_exp") {
+
+        ## vcov.survreg gives an unnamed vcov matrix
+        vcov_ready <- vcov_raw
+        dimnames(vcov_ready) <- list(coef(yreg_fit),
+                                     coef(yreg_fit))
+
+    } else if (yreg == "survAFT_weibull") {
+
+        ## vcov.survreg(weibull_fit) has a scale parameter variance
+        ## as the right lower corner.
+        coef_ind <- seq_along(coef(yreg_fit))
+        vcov_ready <- vcov_raw[coef_ind,coef_ind]
+        dimnames(vcov_ready) <- list(coef(yreg_fit),
+                                     coef(yreg_fit))
+
+    } else if (yreg == "survCox") {
+
+        ## Pad the left and upper edges with zeros by creating a block diagonal.
+        vcov_ready <- Matrix::bdiag(matrix(0), vcov_raw)
+        dimnames(vcov_ready) <- list(coef(yreg_fit),
+                                     coef(yreg_fit))
+
+    } else {
+
+        vcov_ready <- vcov_raw
+
+    }
+
+    if (interaction) {
+        vars <- c("(Intercept)", avar, mvar, paste0(avar,":",mvar), cvar)
+    } else {
+        vars <- c("(Intercept)", avar, mvar,                        cvar)
+    }
+    vcov_ready[vars,vars, drop = FALSE]
+}
+
+sigma_hat_sq <- function(mreg_fit) {
+
+    ## FIXME: This is tentative and suspicious.
+    ## Chapter 2
+    ## Quadratic Forms of Random Variables
+    ## http://pages.stat.wisc.edu/~st849-1/lectures/Ch02.pdf
+    ## Corollary 6. In a full-rank Gaussian model for M with p covariates X.
+    ## ||M - X beta_hat||^2 / sigma^2 ~ (central chi-squared DF = (n - p))
+    ## Var(central chi-squared DF = (n - p)) = (n - p)
+    ## Var(sigma_hat^2) = Var(1/(n-p) * ||M - X beta_hat||^2)
+    ##                  = 1/(n-p)^2 * Var(||M - X beta_hat||^2 * sigma^2/sigma^2)
+    ##                  = 1/(n-p)^2 * (sigma^2)^2 * Var(||M - X beta_hat||^2 /sigma^2)
+    ##                  = 1/(n-p)^2 * (sigma^2)^2 * (n-p)
+    ##                  = (sigma^2)^2 / (n-p)
+    ##
+    ## (sigma_hat^2)^2 / (n-p)
+    matrix(((sigma(mreg_fit))^2)^2 / mreg_fit$df.residual)
+}
+
+
+###
 ### vcov extractors
 ################################################################################
 
