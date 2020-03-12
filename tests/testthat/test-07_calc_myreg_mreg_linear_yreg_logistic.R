535dd6864e942ad0ec3255cb74474c562339fc21
Kazuki Yoshida <kazukiyoshida@mail.harvard.edu>
2020-03-12 16:30:33 -0400

    Add test for calc_myreg_mreg_linear_yreg_logistic_est

--- tests/testthat/test-07_calc_myreg_mreg_linear_yreg_logistic.R
+++ tests/testthat/test-07_calc_myreg_mreg_linear_yreg_logistic.R
@@ -12,18 +12,36 @@ library(tidyverse)
 
 
 ###
-### Effect estimation
+###
 ################################################################################
 
-test_that("calc_myreg_mreg_linear_yreg_logistic_est works given coef", {
+describe("calc_myreg_mreg_linear_yreg_logistic", {
 
 })
 
 
 ###
-### Standard error estimation
+### Effect estimation function constructor
 ################################################################################
 
-test_that("calc_myreg_mreg_linear_yreg_logistic_se works given coef and vcov", {
+describe("calc_myreg_mreg_linear_yreg_logistic_est", {
+
+    est_fun <- calc_myreg_mreg_linear_yreg_logistic_est()
+    res <- est_fun(a0 = 0, a1 = 1, m_cde = 0, c_cond = c(1,2,3))
+
+    it("returns a numeric vector of length 7 with appropriate named elements", {
+        expect_equal(length(res),
+                     7)
+        expect_equal(names(res),
+                     c("cde","pnde","tnie","tnde","pnie","te","pm"))
+    })
+})
+
+
+###
+### Standard error estimation function constructor
+################################################################################
+
+describe("calc_myreg_mreg_linear_yreg_logistic_se", {
 
 })
