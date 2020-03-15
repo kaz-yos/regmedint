4b20da74455809c8d1bd90f7fe0951858f83113a
Kazuki Yoshida <kazukiyoshida@mail.harvard.edu>
2020-03-15 09:21:10 -0400

    Document print.regmedint

--- R/08_regmedint_class_user_methods.R
+++ R/08_regmedint_class_user_methods.R
@@ -12,16 +12,15 @@
 
 ##' print method for regmedint object
 ##'
-##' .. content for \description{} (no empty lines) ..
+##' Print the \code{mreg_fit}, \code{yreg_fit}, and the mediation analysis effect estimates.
 ##'
-##' .. content for \details{} ..
-##'
-##'
-##' @param x
+##' @param x An object of the \code{regmedint} class.
+##' @param m_cde A numeric vector of length one. A mediator value at which the controlled direct effect (CDE) conditional on the adjustment covariates is evaluated. If not provided, the default value supplied to the call to \code{\link{regmedint}} will be used. Only the CDE is affected.
+##' @param c_cond A numeric vector as long as the number of adjustment covariates. A set of covariate values at which the conditional natural effects are evaluated.
 ##' @param ...
 ##'
-##' @return
-print.regmedint <- function(x, ...) {
+##' @return Invisibly return the \code{regmedint} class object as is.
+print.regmedint <- function(x, m_cde = NULL, c_cond = NULL, , ...) {
 
     stop("print.regmedint not implemented")
 
