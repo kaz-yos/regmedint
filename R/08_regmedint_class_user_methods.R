8a55889e218f25a065932a5de1d3eb91a549271d
Kazuki Yoshida <kazukiyoshida@mail.harvard.edu>
2020-03-15 17:50:01 -0400

    Add preliminary version of print.regmedint and summary.regmedint

--- R/08_regmedint_class_user_methods.R
+++ R/08_regmedint_class_user_methods.R
@@ -15,16 +15,50 @@
 ##' Print the \code{mreg_fit}, \code{yreg_fit}, and the mediation analysis effect estimates.
 ##'
 ##' @param x An object of the \code{regmedint} class.
+##' @param a0 A numeric vector of length one.
+##' @param a1 A numeric vector of length one.
 ##' @param m_cde A numeric vector of length one. A mediator value at which the controlled direct effect (CDE) conditional on the adjustment covariates is evaluated. If not provided, the default value supplied to the call to \code{\link{regmedint}} will be used. Only the CDE is affected.
 ##' @param c_cond A numeric vector as long as the number of adjustment covariates. A set of covariate values at which the conditional natural effects are evaluated.
 ##' @param ...
 ##'
 ##' @return Invisibly return the \code{regmedint} class object as is.
-print.regmedint <- function(x, m_cde = NULL, c_cond = NULL, , ...) {
+print.regmedint <- function(x,
+                            a0 = NULL,
+                            a1 = NULL,
+                            m_cde = NULL,
+                            c_cond = NULL,
+                            ...) {
 
-    stop("print.regmedint not implemented")
+    ## This is a user function. Check arguments heavily.
+    assertthat::assert_that(is.null(a0) | (length(a0) == 1))
+    assertthat::assert_that(is.null(a1) | (length(a1) == 1))
+    assertthat::assert_that(is.null(m_cde) | (length(m_cde) == 1))
+    assertthat::assert_that(is.null(c_cond) | (length(c_cond) == length(x$args$cvar)))
 
+    cat("### Mediator model\n")
+    print(x$mreg)
+
+    cat("### Outcome model\n")
+    print(x$yreg)
+
+    cat("### Mediation analysis \n")
+    if (is.null(a0)) {
+        a0 <- x$args$a0
+    }
+    if (is.null(a1)) {
+        a1 <- x$args$a1
+    }
+    if (is.null(m_cde)) {
+        m_cde <- x$args$m_cde
+    }
+    if (is.null(c_cond)) {
+        c_cond <- x$args$c_cond
+    }
+    print(x$myreg$est_fun(a0 = a0, a1 = a1, m_cde = m_cde, c_cond = c_cond))
+
+    invisible(x)
 }
+
 
 ###
 ### summary method
@@ -32,19 +66,63 @@ print.regmedint <- function(x, m_cde = NULL, c_cond = NULL, , ...) {
 
 ##' summary method for regmedint object
 ##'
-##'
-##'
-##' .. content for \details{} ..
-##'
+##' Show summary of each model.
 ##'
 ##' @param x
+##' @param a0
+##' @param a1
+##' @param m_cde
+##' @param c_cond
 ##' @param ...
 ##'
 ##' @return
-summary.regmedint <- function(x, ...) {
+summary.regmedint <- function(x,
+                              a0 = NULL,
+                              a1 = NULL,
+                              m_cde = NULL,
+                              c_cond = NULL,
+                              ...) {
 
-    stop("summary.regmedint not implemented")
+    ## This is a user function. Check arguments heavily.
+    assertthat::assert_that(is.null(a0) | (length(a0) == 1))
+    assertthat::assert_that(is.null(a1) | (length(a1) == 1))
+    assertthat::assert_that(is.null(m_cde) | (length(m_cde) == 1))
+    assertthat::assert_that(is.null(c_cond) | (length(c_cond) == length(x$args$cvar)))
 
+    cat("### Mediator model\n")
+    summary(x$mreg)
+
+    cat("### Outcome model\n")
+    summary(x$yreg)
+
+    cat("### Mediation analysis \n")
+    if (is.null(a0)) {
+        a0 <- x$args$a0
+    }
+    if (is.null(a1)) {
+        a1 <- x$args$a1
+    }
+    if (is.null(m_cde)) {
+        m_cde <- x$args$m_cde
+    }
+    if (is.null(c_cond)) {
+        c_cond <- x$args$c_cond
+    }
+
+    res_est <- x$myreg$est_fun(a0 = a0, a1 = a1, m_cde = m_cde, c_cond = c_cond)
+    res_se <- x$myreg$se_fun(a0 = a0, a1 = a1, m_cde = m_cde, c_cond = c_cond)
+    assertthat::assert_that(length(res_est) == length(res_se))
+    res_Z <- res_est / res_se
+    res_p <- pnorm(q = res_Z)
+
+    res_mat <- cbind(estimate = res_est,
+                     estimate = res_se,
+                     Z = res_Z,
+                     p = res_p)
+
+    print(res_mat, quote = FALSE)
+
+    invisible(res_mat)
 }
 
 
