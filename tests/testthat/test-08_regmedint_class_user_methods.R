47b53df0545941b99d37699ccac0ebe4f4b4a020
Kazuki Yoshida <kazukiyoshida@mail.harvard.edu>
2020-03-16 06:06:50 -0400

    Add test shells for user methods

---
+++ tests/testthat/test-08_regmedint_class_user_methods.R
@@ -0,0 +1,18 @@
+################################################################################
+### Tests for the user methods for the regmedint class
+##
+## Created on: 2020-03-16
+## Author: Kazuki Yoshida
+################################################################################
+
+###
+### regmedint class
+################################################################################
+
+describe("print.regmedint", {})
+
+describe("summary.regmedint", {})
+
+describe("coef.regmedint", {})
+
+describe("confint.regmedint, {}")
