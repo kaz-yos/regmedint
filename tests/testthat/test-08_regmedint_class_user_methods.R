721b5c06dfaf7c3b6d03a0f231a931d2cbc1a7e8
Kazuki Yoshida <kazukiyoshida@mail.harvard.edu>
2020-03-16 22:36:06 -0400

    Add tentative tests for user methods

--- tests/testthat/test-08_regmedint_class_user_methods.R
+++ tests/testthat/test-08_regmedint_class_user_methods.R
@@ -9,10 +9,69 @@
 ### regmedint class
 ################################################################################
 
-describe("print.regmedint", {})
+describe("methods for regmedint", {
 
-describe("summary.regmedint", {})
+    data(pbc)
+    ## Missing data should be warned in validate_args()
+    pbc_cc <- pbc[complete.cases(pbc),] %>%
+        mutate(male = if_else(sex == "m", 1L, 0L),
+               ## Combine transplant and death for testing purpose
+               status = if_else(status == 0, 0L, 1L))
 
-describe("coef.regmedint", {})
-
-describe("confint.regmedint, {}")
+    fit_mreg_lin_yreg_lin <- regmedint(data = pbc_cc,
+                                       yvar = "alk.phos",
+                                       avar = "trt",
+                                       mvar = "bili",
+                                       cvar = NULL,
+                                       a0 = 1,
+                                       a1 = 2,
+                                       m_cde = 0,
+                                       c_cond = NULL,
+                                       mreg = "linear",
+                                       yreg = "linear",
+                                       interaction = FALSE,
+                                       casecontrol = FALSE,
+                                       eventvar = NULL)
+    ##
+    describe("print.regmedint", {
+        it("prints results with expected elements", {
+            expect_output(print(fit_mreg_lin_yreg_lin), "cde")
+            expect_output(print(fit_mreg_lin_yreg_lin), "pnde")
+            expect_output(print(fit_mreg_lin_yreg_lin), "tnie")
+            expect_output(print(fit_mreg_lin_yreg_lin), "tnde")
+            expect_output(print(fit_mreg_lin_yreg_lin), "pnie")
+            expect_output(print(fit_mreg_lin_yreg_lin), "te")
+            expect_output(print(fit_mreg_lin_yreg_lin), "pm")
+        })
+    })
+    ##
+    describe("summary.regmedint", {
+            expect_output(summary(fit_mreg_lin_yreg_lin), "cde")
+            expect_output(summary(fit_mreg_lin_yreg_lin), "pnde")
+            expect_output(summary(fit_mreg_lin_yreg_lin), "tnie")
+            expect_output(summary(fit_mreg_lin_yreg_lin), "tnde")
+            expect_output(summary(fit_mreg_lin_yreg_lin), "pnie")
+            expect_output(summary(fit_mreg_lin_yreg_lin), "te")
+            expect_output(summary(fit_mreg_lin_yreg_lin), "pm")
+    })
+    ##
+    describe("coef.regmedint", {
+            expect_output(coef(fit_mreg_lin_yreg_lin), "cde")
+            expect_output(coef(fit_mreg_lin_yreg_lin), "pnde")
+            expect_output(coef(fit_mreg_lin_yreg_lin), "tnie")
+            expect_output(coef(fit_mreg_lin_yreg_lin), "tnde")
+            expect_output(coef(fit_mreg_lin_yreg_lin), "pnie")
+            expect_output(coef(fit_mreg_lin_yreg_lin), "te")
+            expect_output(coef(fit_mreg_lin_yreg_lin), "pm")
+    })
+    ##
+    describe("confint.regmedint", {
+            expect_output(confint(fit_mreg_lin_yreg_lin), "cde")
+            expect_output(confint(fit_mreg_lin_yreg_lin), "pnde")
+            expect_output(confint(fit_mreg_lin_yreg_lin), "tnie")
+            expect_output(confint(fit_mreg_lin_yreg_lin), "tnde")
+            expect_output(confint(fit_mreg_lin_yreg_lin), "pnie")
+            expect_output(confint(fit_mreg_lin_yreg_lin), "te")
+            expect_output(confint(fit_mreg_lin_yreg_lin), "pm")
+    })
+})
