eab7d56a9ef70b5042f0393930390b5d4e85f32e
Kazuki Yoshida <kazukiyoshida@mail.harvard.edu>
2020-03-16 22:03:53 -0400

    Reorganize arguments for regmedint

--- R/01_regmedint_class_ui.R
+++ R/01_regmedint_class_ui.R
@@ -32,11 +32,11 @@
 ##' @param a0 A numeric vector of length 1. Reference level of treatment variable that is considered "untreated" or "unexposed".
 ##' @param a1 A numeric vector of length 1.
 ##' @param m_cde A numeric vector of length 1. Mediator level at which controlled direct effect is evaluated at.
-##' @param yreg A character vector of length 1. Outcome regression type. One of
+##' @param c_cond A numeric vector of the same length as cvar. Required for the ful output. The conditional effect
 ##' @param mreg A character vector of length 1. Outcome regression type. One of "linear"
+##' @param yreg A character vector of length 1. Outcome regression type. One of
 ##' @param interaction A logical vector of length 1. Default to TRUE. Whether to include a mediator-treatment interaction term in the outcome regression model.
 ##' @param casecontrol A logical vector of length 1. Default to FALSE. Whether data comes from a case-control study.
-##' @param c_cond A numeric vector of the same length as cvar. Required for the ful output. The conditional effect
 ##' @param eventvar An character vector of length 1. Only required for survival outcome regression models. Note that the coding is 1 for event and 0 for censoring, following the R survival package convention.
 ##'
 ##' @return regmedint object, which is a list containing the mediator regression object, the outcome regression object, and the regression-based mediation results.
@@ -50,49 +50,48 @@ regmedint <- function(data,
                       a0,
                       a1,
                       m_cde,
-                      yreg,
+                      c_cond,
                       mreg,
+                      yreg,
                       interaction = TRUE,
                       casecontrol = FALSE,
-                      c_cond = NULL,
                       eventvar = NULL) {
     ## This is the user-friendly helper function with a name that is the class name.
     ## https://adv-r.hadley.nz/s3.html#helpers
 
     ## Check data contains yvar, avar, mvar, cvar, eventvar (if provided)
-    validate_args(data,
-                  yvar,
-                  avar,
-                  mvar,
-                  cvar,
-                  a0,
-                  a1,
-                  m_cde,
-                  yreg,
-                  mreg,
-                  interaction,
-                  casecontrol,
-                  full_output,
-                  c_cond,
-                  eventvar)
+    validate_args(data = data,
+                  yvar = yvar,
+                  avar = avar,
+                  mvar = mvar,
+                  cvar = cvar,
+                  a0 = a0,
+                  a1 = a1,
+                  m_cde = m_cde,
+                  c_cond = c_cond,
+                  mreg = mreg,
+                  yreg = yreg,
+                  interaction = interaction,
+                  casecontrol = casecontrol,
+                  eventvar = eventvar)
 
     ## Construct a regmedint object after argument validation.
     ## This is the low-level constructor function.
     ## https://adv-r.hadley.nz/s3.html#s3-constructor
-    res <- new_regmedint(data,
-                         yvar,
-                         avar,
-                         mvar,
-                         cvar,
-                         a0,
-                         a1,
-                         m_cde,
-                         yreg,
-                         mreg,
-                         interaction,
-                         casecontrol,
-                         c_cond,
-                         eventvar)
+    res <- new_regmedint(data = data,
+                         yvar = yvar,
+                         avar = avar,
+                         mvar = mvar,
+                         cvar = cvar,
+                         a0 = a0,
+                         a1 = a1,
+                         m_cde = m_cde,
+                         c_cond = c_cond,
+                         mreg = mreg,
+                         yreg = yreg,
+                         interaction = interaction,
+                         casecontrol = casecontrol,
+                         eventvar = eventvar)
 
     ## Check the resulting object for anomalies.
     ## This is the class validator function.
@@ -113,7 +112,7 @@ regmedint <- function(data,
 ##'
 ##' @inheritParams regmedint
 ##'
-##' @return None
+##' @return NULL
 validate_args <- function(data,
                           yvar,
                           avar,
@@ -122,16 +121,56 @@ validate_args <- function(data,
                           a0,
                           a1,
                           m_cde,
-                          yreg,
+                          c_cond,
                           mreg,
+                          yreg,
                           interaction,
                           casecontrol,
-                          full_output,
-                          c_cond,
-                          boot,
                           eventvar) {
 
-    stop("Unimplemented!
-Need to validate arguments before passing to other functions!")
+    assertthat::assert_that(is.data.frame(data))
+    ##
+    assertthat::assert_that(is.character(yvar))
+    assertthat::assert_that(length(yvar) == 1)
+    ##
+    assertthat::assert_that(is.character(avar))
+    assertthat::assert_that(length(avar) == 1)
+    ##
+    assertthat::assert_that(is.character(mvar))
+    assertthat::assert_that(length(mvar) == 1)
+    ##
+    assertthat::assert_that(is.character(cvar))
+    ##
+    assertthat::assert_that(is.numeric(a0))
+    assertthat::assert_that(length(a0) == 1)
+    ##
+    assertthat::assert_that(is.numeric(a1))
+    assertthat::assert_that(length(a1) == 1)
+    ##
+    assertthat::assert_that(is.numeric(m_cde))
+    assertthat::assert_that(length(m_cde) == 1)
+    ##
+    assertthat::assert_that(is.numeric(c_cond))
+    assertthat::assert_that(length(c_cond) == length(cvar))
+    ##
+    assertthat::assert_that(is.character(mreg))
+    assertthat::assert_that(length(mreg) == 1)
+    assertthat::assert_that(mreg %in% c("linear","logistic"))
+    ##
+    assertthat::assert_that(is.character(yreg))
+    assertthat::assert_that(length(yreg) == 1)
+    assertthat::assert_that(yreg %in% c("linear",
+                                        "logistic","loglinear","poisson","negbin",
+                                        "survCox","survAFT_exp","survAFT_weibull"))
+    ##
+    assertthat::assert_that(is.logical(interaction))
+    assertthat::assert_that(length(interaction) == 1)
+    ##
+    assertthat::assert_that(is.logical(casecontrol))
+    assertthat::assert_that(length(casecontrol) == 1)
+    ##
+    assertthat::assert_that(is.character(eventvar))
+    assertthat::assert_that(length(eventvar) == 1)
 
+    NULL
 }
