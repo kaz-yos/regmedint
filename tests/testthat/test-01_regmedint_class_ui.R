170780e9252a0f268e81416f4f551c58b0ee5e64
Kazuki Yoshida <kazukiyoshida@mail.harvard.edu>
2020-03-18 03:54:53 -0400

    Add tests for regmedint (just running them now)

--- tests/testthat/test-01_regmedint_class_ui.R
+++ tests/testthat/test-01_regmedint_class_ui.R
@@ -22,20 +22,41 @@ describe("regmedint", {
                ## Combine transplant and death for testing purpose
                status = if_else(status == 0, 0L, 1L))
 
-    it("runs", {
-        fit_regmedint <- regmedint(data = pbc_cc,
-                                   yvar = "alk.phos",
-                                   avar = "trt",
-                                   mvar = "bili",
-                                   cvar = NULL,
-                                   a0 = 1,
-                                   a1 = 2,
-                                   m_cde = 0,
-                                   c_cond = NULL,
-                                   mreg = "linear",
-                                   yreg = "linear",
-                                   interaction = FALSE,
-                                   casecontrol = FALSE,
-                                   eventvar = NULL)
+    describe("regmedint mreg linear yreg linear", {
+        it("runs witmh zero cvar with no interaction", {
+            fit_regmedint <- regmedint(data = pbc_cc,
+                                       yvar = "alk.phos",
+                                       avar = "trt",
+                                       mvar = "bili",
+                                       cvar = NULL,
+                                       a0 = 1,
+                                       a1 = 2,
+                                       m_cde = 0,
+                                       c_cond = NULL,
+                                       mreg = "linear",
+                                       yreg = "linear",
+                                       interaction = FALSE,
+                                       casecontrol = FALSE,
+                                       eventvar = NULL)
+        })
+    })
+    ##
+    describe("regmedint mreg linear yreg logistic", {
+        it("runs witmh zero cvar with no interaction", {
+            fit_regmedint <- regmedint(data = pbc_cc,
+                                       yvar = "spiders",
+                                       avar = "trt",
+                                       mvar = "bili",
+                                       cvar = NULL,
+                                       a0 = 1,
+                                       a1 = 2,
+                                       m_cde = 0,
+                                       c_cond = NULL,
+                                       mreg = "linear",
+                                       yreg = "logistic",
+                                       interaction = FALSE,
+                                       casecontrol = FALSE,
+                                       eventvar = NULL)
+        })
     })
 })
