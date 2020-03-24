53a9a7d925d89bb343e4174c3a8408dec98cc51f
kaz-yos <kazukiyoshida@mail.harvard.edu>
2020-03-24 04:53:46 -0400

    Create a macro call programatically

--- tests/reference_results/02_generate_sas_macro_calls.R
+++ tests/reference_results/02_generate_sas_macro_calls.R
@@ -29,66 +29,7 @@ library(tidyverse)
 
 cat("
 ###
-### Generate all relevant patterns of %mediation() calls
-################################################################################\n")
-
-macro_args <- crossing(mreg = c("linear",
-                                "logistic"),
-                       yreg = c("linear",
-                                "logistic",
-                                "loglinear",
-                                "poisson",
-                                "negbin",
-                                "survCox",
-                                "survAFT_exp",
-                                "survAFT_weibull"),
-                       interaction = c("false","true"),
-                       casecontrol = c("false","true"),
-                       ncvar = c(0,3)) %>%
-    ## casecontrol is only relevant for binary yreg
-    filter(casecontrol == "false" |
-           (casecontrol == "true" & (yreg == "logistic" | yreg == "loglinear"))) %>%
-    ## Choose appropriate variable names
-    mutate(avar = "trt",
-           ##
-           mvar = case_when(mreg == "linear" ~ "bili",
-                            mreg == "logistic" ~ "bili_bin"),
-           ##
-           yvar = case_when(yreg == "linear" ~ "alk_phos",
-                            ##
-                            yreg == "logistic" ~ "spiders",
-                            yreg == "loglinear" ~ "spiders",
-                            ##
-                            yreg == "poisson" ~ "edema",
-                            yreg == "negbin" ~ "edema",
-                            ##
-                            yreg == "survCox" ~ "time",
-                            yreg == "survAFT_exp" ~ "time",
-                            yreg == "survAFT_weibull" ~ "time"),
-           ##
-           eventvar = case_when(yreg == "survCox" ~ "status",
-                                yreg == "survAFT_exp" ~ "status",
-                                yreg == "survAFT_weibull" ~ "status",
-                                TRUE ~ ""),
-           ##
-           cvar = case_when(ncvar == 0 ~ "",
-                            ncvar == 3 ~ "age male stage"),
-           ##
-           c_cond = case_when(ncvar == 0 ~ "",
-                              ncvar == 3 ~ "50 1 2")) %>%
-    ## File name
-    mutate(filename = sprintf("sas-mreg_%s_yreg_%s_int_%s_caco_%s.sas",
-                              mreg,
-                              yreg,
-                              str_sub(interaction, start = 1, end = 1),
-                              str_sub(casecontrol, start = 1, end = 1)))
-macro_args %>%
-    print(n = Inf)
-
-
-cat("
-###
-### Construct macro calls
+### Construct macro call templates
 ################################################################################\n")
 
 common_string_ <- "
@@ -125,6 +66,91 @@ macro_call_string_fmt <- "
     boot = ,
     cens = %s);
 run;"
+
+
+cat("
+###
+### Generate all relevant patterns of %mediation() calls
+################################################################################\n")
+
+macro_args <-
+    crossing(mreg = c("linear",
+                      "logistic"),
+             yreg = c("linear",
+                      "logistic",
+                      "loglinear",
+                      "poisson",
+                      "negbin",
+                      "survCox",
+                      "survAFT_exp",
+                      "survAFT_weibull"),
+             interaction = c("false","true"),
+             casecontrol = c("false","true"),
+             ncvar = c(0,3)) %>%
+    ## casecontrol is only relevant for binary yreg
+    filter(casecontrol == "false" |
+           (casecontrol == "true" & (yreg == "logistic" | yreg == "loglinear"))) %>%
+    ## Choose appropriate variable names
+    mutate(avar = "trt",
+           ##
+           mvar = case_when(mreg == "linear" ~ "bili",
+                            mreg == "logistic" ~ "bili_bin"),
+           ##
+           yvar = case_when(yreg == "linear" ~ "alk_phos",
+                            ##
+                            yreg == "logistic" ~ "spiders",
+                            yreg == "loglinear" ~ "spiders",
+                            ##
+                            yreg == "poisson" ~ "edema",
+                            yreg == "negbin" ~ "edema",
+                            ##
+                            yreg == "survCox" ~ "time",
+                            yreg == "survAFT_exp" ~ "time",
+                            yreg == "survAFT_weibull" ~ "time"),
+           ##
+           eventvar = case_when(yreg == "survCox" ~ "status",
+                                yreg == "survAFT_exp" ~ "status",
+                                yreg == "survAFT_weibull" ~ "status",
+                                TRUE ~ ""),
+           cens = "cens",
+           ##
+           cvar = case_when(ncvar == 0 ~ "",
+                            ncvar == 3 ~ "age male stage"),
+           ##
+           c_cond = case_when(ncvar == 0 ~ "",
+                              ncvar == 3 ~ "50 1 2"),
+           a0 = "1",
+           a1 = "2",
+           m_cde = "1") %>%
+    ## Contruct macro text
+    mutate(macro_call = sprintf(macro_call_string_fmt,
+                                yvar,
+                                avar,
+                                mvar,
+                                cvar,
+                                a0,
+                                a1,
+                                m_cde,
+                                yreg,
+                                mreg,
+                                interaction,
+                                casecontrol,
+                                c_cond,
+                                cens)) %>%
+    ## File name
+    mutate(filename = sprintf("sas-mreg_%s_yreg_%s_int_%s_caco_%s_ncvar%s.sas",
+                              ## All variables in crossing() should appear hear.
+                              mreg,
+                              yreg,
+                              str_sub(interaction, start = 1, end = 1),
+                              str_sub(casecontrol, start = 1, end = 1),
+                              ncvar))
+macro_args %>%
+    print(n = Inf)
+
+macro_args %>%
+    select(filename) %>%
+    print(n = Inf)
 
 
 
