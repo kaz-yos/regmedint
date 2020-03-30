8d095237630abef0d22496ae99adfc3e657a58d9
kaz-yos <kazukiyoshida@mail.harvard.edu>
2020-03-29 22:28:58 -0400

    Add data check tests

--- tests/testthat/test-01_regmedint_class_ui.R
+++ tests/testthat/test-01_regmedint_class_ui.R
@@ -23,6 +23,43 @@ describe("regmedint", {
                ## Combine transplant and death for testing purpose
                status = if_else(status == 0, 0L, 1L))
 
+    describe("regmedint argument validation", {
+        it("rejects missing data in the variales of interest", {
+            expect_error(regmedint(data = pbc,
+                                   yvar = "alk.phos",
+                                   avar = "trt",
+                                   mvar = "bili",
+                                   cvar = NULL,
+                                   a0 = 1,
+                                   a1 = 2,
+                                   m_cde = 0,
+                                   c_cond = NULL,
+                                   mreg = "linear",
+                                   yreg = "linear",
+                                   interaction = FALSE,
+                                   casecontrol = FALSE,
+                                   eventvar = NULL),
+                         "Missing is not allowed! Consider multiple imputation.")
+        })
+        it("rejects factor variables on the right hand side", {
+            expect_error(regmedint(data = pbc,
+                                   yvar = "alk.phos",
+                                   avar = "trt",
+                                   mvar = "bili",
+                                   cvar = "sex",
+                                   a0 = 1,
+                                   a1 = 2,
+                                   m_cde = 0,
+                                   c_cond = "f",
+                                   mreg = "linear",
+                                   yreg = "linear",
+                                   interaction = FALSE,
+                                   casecontrol = FALSE,
+                                   eventvar = NULL),
+                         "Factor variables are not allowed! Use numeric variables only.")
+        })
+    })
+
     describe("regmedint mreg linear yreg linear", {
         it("runs with zero cvar with no interaction", {
             fit_regmedint <- regmedint(data = pbc_cc,
