bd4e56936ba5d0b0bb2f8374fd4f3255d6bc0500
kaz-yos <kazukiyoshida@mail.harvard.edu>
2020-04-22 06:41:05 -0400

    Add summary.regmedint_mod_poisson as a copy of summary.glm
    Taken from https://github.com/wch/r-source/blob/trunk/src/library/stats/R/glm.R

--- R/04_fit_yreg_outcome_modeling.R
+++ R/04_fit_yreg_outcome_modeling.R
@@ -198,3 +198,97 @@ string_yreg_formula <- function(yvar,
 vcov.regmedint_mod_poisson <- function(object, ...) {
     sandwich::sandwich(object, ...)
 }
+
+
+##' Summary with robust sandwich variance estimator for modified Poisson
+##'
+##' This is a version of \code{\link{summary.glm}} modified to use the robust variance estimator \code{\link[sandwich]{sandwich}}.
+##'
+##' @param object A model object of the class \code{regmedint_mod_poisson}
+##' @param ... For compatibility with the generic.
+##'
+##' @return An object of the class \code{summary.glm}
+summary.regmedint_mod_poisson <- function(object, ...) {
+    ## https://github.com/wch/r-source/blob/trunk/src/library/stats/R/glm.R
+    est.disp <- FALSE
+    df.r <- object$df.residual
+    if(is.null(dispersion))	# calculate dispersion if needed
+	dispersion <-
+	    if(object$family$family %in% c("poisson", "binomial"))  1
+	    else if(df.r > 0) {
+                est.disp <- TRUE
+		if(any(object$weights==0))
+		    warning("observations with zero weight not used for calculating dispersion")
+		sum((object$weights*object$residuals^2)[object$weights > 0])/ df.r
+	    } else {
+                est.disp <- TRUE
+                NaN
+            }
+
+    ## calculate scaled and unscaled covariance matrix
+
+    aliased <- is.na(coef(object))  # used in print method
+    p <- object$rank
+    if (p > 0) {
+        p1 <- 1L:p
+	Qr <- qr.lm(object)
+        ## WATCHIT! doesn't this rely on pivoting not permuting 1L:p? -- that's quaranteed
+        coef.p <- object$coefficients[Qr$pivot[p1]]
+        covmat.unscaled <- chol2inv(Qr$qr[p1,p1,drop=FALSE])
+        dimnames(covmat.unscaled) <- list(names(coef.p),names(coef.p))
+        covmat <- dispersion*covmat.unscaled
+        var.cf <- diag(covmat)
+
+        ## calculate coef table
+
+        s.err <- sqrt(var.cf)
+        tvalue <- coef.p/s.err
+
+        dn <- c("Estimate", "Std. Error")
+        if(!est.disp) { # known dispersion
+            pvalue <- 2*pnorm(-abs(tvalue))
+            coef.table <- cbind(coef.p, s.err, tvalue, pvalue)
+            dimnames(coef.table) <- list(names(coef.p),
+                                         c(dn, "z value","Pr(>|z|)"))
+        } else if(df.r > 0) {
+            pvalue <- 2*pt(-abs(tvalue), df.r)
+            coef.table <- cbind(coef.p, s.err, tvalue, pvalue)
+            dimnames(coef.table) <- list(names(coef.p),
+                                         c(dn, "t value","Pr(>|t|)"))
+        } else { # df.r == 0
+            coef.table <- cbind(coef.p, NaN, NaN, NaN)
+            dimnames(coef.table) <- list(names(coef.p),
+                                         c(dn, "t value","Pr(>|t|)"))
+        }
+        df.f <- NCOL(Qr$qr)
+    } else {
+        coef.table <- matrix(, 0L, 4L)
+        dimnames(coef.table) <-
+            list(NULL, c("Estimate", "Std. Error", "t value", "Pr(>|t|)"))
+        covmat.unscaled <- covmat <- matrix(, 0L, 0L)
+        df.f <- length(aliased)
+    }
+    ## return answer
+
+    ## these need not all exist, e.g. na.action.
+    keep <- match(c("call","terms","family","deviance", "aic",
+                    "contrasts", "df.residual","null.deviance","df.null",
+                    "iter", "na.action"), names(object), 0L)
+    ans <- c(object[keep],
+	     list(deviance.resid = residuals(object, type = "deviance"),
+		  coefficients = coef.table,
+                  aliased = aliased,
+		  dispersion = dispersion,
+		  df = c(object$rank, df.r, df.f),
+		  cov.unscaled = covmat.unscaled,
+		  cov.scaled = covmat))
+
+    if(correlation && p > 0) {
+	dd <- sqrt(diag(covmat.unscaled))
+	ans$correlation <-
+	    covmat.unscaled/outer(dd,dd)
+	ans$symbolic.cor <- symbolic.cor
+    }
+    class(ans) <- "summary.glm"
+    return(ans)
+}
