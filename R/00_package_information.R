cd175e6d818ac8330917276a37d224e77d49d294
kaz-yos <kazukiyoshida@mail.harvard.edu>
2020-05-07 05:10:06 -0400

    Document vv2015 data object

--- R/00_package_information.R
+++ R/00_package_information.R
@@ -26,3 +26,24 @@
 ##' @docType package
 ##' @name regmedint
 NULL
+
+
+## http://r-pkgs.had.co.nz/data.html#data-data
+
+##' Example dataset from Valeri and VanderWeele 2015.
+##'
+##' An example dataset from Valeri and VanderWeele (2015) <doi:10.1097/EDE.0000000000000253>.
+##'
+##' @format A tibble with 100 rows and 7 variables:
+##' \describe{
+##'   \item{id}{Positive integer id.}
+##'   \item{x}{Binary treatment assignment variable.}
+##'   \item{m}{Binary mediator variable.}
+##'   \item{y}{Time to event outcome variable.}
+##'   \item{cens}{Binary censoring indicator. Censored is 1.}
+##'   \item{c}{Continuous confounder variable.}
+##'   \item{event}{Binary event indicator. Event is 1.}
+##' }
+##'
+##' @source \url{https://www.hsph.harvard.edu/tyler-vanderweele/tools-and-tutorials/}
+"vv2015"
